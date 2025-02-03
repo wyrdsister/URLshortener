a5860b2c0261c5a1a81cc414d7ebac67c030e1f9
wyrdsister <elizabethvalerivna97@gmail.com>
2025-02-03 19:31:35 +0100

    initial commit

---
+++ src/main/kotlin/wyrd/sister/URLshortener/UrLshortenerApplication.kt
@@ -0,0 +1,11 @@
+package wyrd.sister.URLshortener
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrLshortenerApplication
+
+fun main(args: Array<String>) {
+	runApplication<UrLshortenerApplication>(*args)
+}
---
+++ src/test/kotlin/wyrd/sister/URLshortener/UrLshortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package wyrd.sister.URLshortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrLshortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
